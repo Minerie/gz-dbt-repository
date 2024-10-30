616e248993bdd4c17c1775ad03c12b87a7e805cd
mineriewagon@gmail.com <mineriewagon@gmail.com>
2024-10-30 15:42:17 +0000

    .

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,15 @@
+Select
+    *
+From {{ref("stg_raw__adword")}}
+Union all
+Select
+    *
+From {{ref("stg_raw__bing")}}
+Union all
+Select
+    *
+From {{ref("stg_raw__criteo")}}
+Union all
+Select
+    *
+From {{ref("stg_raw__facebook")}}
