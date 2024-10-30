93b34c80630d6a8c385a40baeff2d31d12dded1b
mineriewagon@gmail.com <mineriewagon@gmail.com>
2024-10-30 09:58:34 +0000

    purchase cost and margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,6 +5,8 @@ select
     quantity,
     purchase_price,
     sales.products_id,
+    Round(quantity * purchase_price) AS purchase_cost,
+    Round(revenue - Round(quantity * purchase_price)) AS margin,
 from {{ref("stg_raw__sales")}} AS sales
 inner join {{ref("stg_raw__product")}} AS product
 on product.products_id = sales.products_id
